6515031337f7d4b5e0ab202614c4d339f0d478a5
Kylla Harris <kylla.harris@gmail.com>
2019-01-09 17:27:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,3 +9,23 @@ end
 def first_element(array)
   first_element[1]
 end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+def first_element_with_array_methods
+  
+end
+
+def last_element_with_array_methods
+  
+end
+
+def length_of_array
+  
+end
